5fd107764a98803444405f11f9890abc821a839f
Dennis de Klerk <dmdeklerk@gmail.com>
2017-09-21 01:39:39 +0200

    Fix import

--- test/builder.test.ts
+++ test/builder.test.ts
@@ -1,8 +1,9 @@
-import { heatsdk } from "../src/heat-sdk"
+import * as heatsdk from "../src/heat-sdk"
 import { Builder } from "../src/builder"
 
 describe("Transaction builder", () => {
   it("can be created", () => {
-    expect(heatsdk.createTransactionBuilder()).toBeInstanceOf(Builder)
+    expect(heatsdk.default.createTransactionBuilder()).toBeInstanceOf(Builder)
   })
+  it("can create a payment", () => {})
 })
