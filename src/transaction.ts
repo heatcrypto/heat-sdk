712b0eee3ae45d6ac7dbe487218d2fba86b0a088
Dennis de Klerk <dmdeklerk@gmail.com>
2017-09-21 03:45:56 +0200

    Transaction builder wrapper

---
+++ src/transaction.ts
@@ -0,0 +1,129 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2017 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+import { Builder, TransactionImpl } from "./builder"
+import * as utils from "./utils"
+import * as appendix from "./appendix"
+import * as converters from "./converters"
+import * as crypto from "./crypto"
+
+export class Transaction {
+  private publicMessage_: string
+  private privateMessage_: string
+  private privateMessageToSelf_: string
+  private messageIsBinary_: boolean
+  private deadline_: number
+  private transaction_: TransactionImpl
+
+  constructor(
+    private recipientOrRecipientPublicKey: string,
+    private builder: Builder
+  ) {}
+
+  public sign(secretPhrase: string): Transaction {
+    this.build()
+    this.transaction_ = this.builder.build(secretPhrase)
+    return this
+  }
+
+  public broadcast() {
+    if (!utils.isDefined(this.transaction_))
+      throw new Error("Must call sign() first")
+  }
+
+  public getTransactionBytes() {
+    if (!utils.isDefined(this.transaction_))
+      throw new Error("Must call sign() first")
+    return this.transaction_.getBytesAsHex()
+  }
+
+  private build() {
+    this.builder.deadline(
+      utils.isDefined(this.deadline_) ? this.deadline_ : 1440
+    )
+    let recipientPublicKeyHex = utils.isPublicKey(
+      this.recipientOrRecipientPublicKey
+    )
+      ? this.recipientOrRecipientPublicKey
+      : null
+    if (recipientPublicKeyHex) {
+      let a = new appendix.AppendixPublicKeyAnnouncement()
+      a.create(converters.hexStringToByteArray(recipientPublicKeyHex))
+      this.builder.publicKeyAnnouncement(a)
+      this.builder.recipientId(
+        crypto.getAccountIdFromPublicKey(recipientPublicKeyHex)
+      )
+    } else {
+      this.builder.recipientId(this.recipientOrRecipientPublicKey)
+    }
+
+    if (utils.isDefined(this.publicMessage_)) {
+      let a = new appendix.AppendixMessage()
+      if (this.messageIsBinary_)
+        a.create(converters.hexStringToByteArray(this.publicMessage_), true)
+      else a.create(converters.stringToByteArray(this.publicMessage_), false)
+      this.builder.message(a)
+    }
+    // else if (utils.isDefined(this.privateMessage_)) {
+    //   let a = new appendix.AppendixEncryptedMessage()
+    //   if (this.messageIsBinary_)
+    //     a.create(converters.hexStringToByteArray(this.publicMessage_), true)
+    //   else
+    //     a.create(converters.stringToByteArray(this.publicMessage_), false)
+    //   this.builder.encryptedMessage(a)
+    // }
+  }
+
+  private hasMessage() {
+    return (
+      utils.isDefined(this.publicMessage_) ||
+      utils.isDefined(this.privateMessage_) ||
+      utils.isDefined(this.privateMessageToSelf_)
+    )
+  }
+
+  public publicMessage(message: string, isBinary?: boolean) {
+    if (this.hasMessage()) throw new Error("Transaction already has a message")
+    this.messageIsBinary_ = !!isBinary
+    this.publicMessage_ = message
+    return this
+  }
+
+  public privateMessage(message: string, isBinary?: boolean) {
+    if (this.hasMessage()) throw new Error("Transaction already has a message")
+    this.messageIsBinary_ = !!isBinary
+    this.privateMessage_ = message
+    return this
+  }
+
+  public privateMessageToSelf(message: string, isBinary?: boolean) {
+    if (this.hasMessage()) throw new Error("Transaction already has a message")
+    this.messageIsBinary_ = !!isBinary
+    this.privateMessageToSelf_ = message
+    return this
+  }
+
+  public deadline(deadline: number) {
+    this.deadline_ = deadline
+    return this
+  }
+}
