f00c6d4abc3d0171d0e8178d059cf3be6cc24bca
Dennis de Klerk <dmdeklerk@gmail.com>
2017-10-13 03:49:43 +0200

    fix(built): Fix rollup/fetch browser built

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,72 @@
 import resolve from "rollup-plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
 import sourceMaps from "rollup-plugin-sourcemaps"
+import globals from "rollup-plugin-node-globals"
 const pkg = require("./package.json")
 const camelCase = require("lodash.camelcase")
-// npm install rollup-plugin-uglify --save-dev
-//import uglify from 'rollup-plugin-uglify';
-// npm install uglify-es --save-dev
-//import { minify } from 'uglify-es';
+import json from 'rollup-plugin-json'
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
 
 const libraryName = "heat-sdk"
-
-export default {
-  input: `compiled/${libraryName}.js`,
-  output: [
-    { file: pkg.main, name: 'heatsdk', format: "umd" }, //camelCase(libraryName)
-    { file: pkg.module, format: "es" }
-  ],
-  sourcemap: true,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: "compiled/**"
+export default [
+  // heat-sdk.js
+  {
+    input: `compiled/${libraryName}.js`,
+    plugins: [
+      json(),
+      resolve({
+        preferBuiltins: true
+      }),
+      commonjs(),
+      sourceMaps()
+    ],
+    sourcemap: true,
+    external: ['fs', 'path', 'events', 'module'],
+    output: [
+			{ file: 'dist/heat-sdk.js', format: 'es' }
+    ],
+    moduleContext: {
+    }
   },
-  plugins: [
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-    /* resolve({
-       jsnext: true,
-       main: true,
-       browser: true,
-    }),*/
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Resolve source maps to the original source
-    sourceMaps(),
-
-    // // Generate minified source bundle for use in browsers
-    //uglify({}, minify)
-  ]
-}
+  // heat-sdk.umd.js
+  {
+    input: `compiled/${libraryName}.js`,
+    plugins: [
+      json(),
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      }),
+      commonjs({
+        ignoreGlobal: true
+      }),
+      globals(),
+      sourceMaps()      
+    ],
+    output: [
+			{ file: 'dist/heat-sdk.browser.js', format: 'umd', name: 'heatsdk' }
+    ]
+  },
+  // heat-sdk.umd.min.js
+  {
+    input: `compiled/${libraryName}.js`,
+    plugins: [
+      json(),
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      }),
+      commonjs({
+        ignoreGlobal: true
+      }),
+      globals(),
+      sourceMaps(),
+      uglify({}, minify)
+    ],
+    sourcemap: true,
+    output: [
+			{ file: 'dist/heat-sdk.browser.min.js', format: 'umd', name: 'heatsdk' }
+    ]
+  }  
+]
